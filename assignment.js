1db8bf173eacadc9d30b3b211aade09c1b7dbd47
TekniqueJoseph <joejacksun@gmail.com>
2023-04-29 13:08:20 -0400

    work for question 1

--- assignment.js
+++ assignment.js
@@ -1,5 +1,29 @@
+//Question 1
 function sayHello(name) {
   console.log('Hi ' + name);
 }
+// sayHello('Joseph');
 
-sayHello();
+const greet1 = (name) => {
+  console.log(`Hi ${name}`)
+}
+greet1('Candace')
+
+// const greet = ((name) => {
+//   console.log(`Hi ${name}`)
+// }) 
+// greet('Candace')
+
+// Question 2
+const greet2a = (salute, name) => {
+  console.log('2a', `${salute} ${name}`)
+}
+greet2a('Hey','Candace')
+
+const greet2b = () => {
+  let salute = 'Hello'
+  let name = 'Candy'
+  console.log('2b',`${salute} ${name}`)
+}
+greet2b()
+
